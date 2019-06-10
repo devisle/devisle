74fea122bf3f13ca9b11d0ec5704cf5bdb0efc5a
Jonathan Prada <jonathanprada@protonmail.ch>
2019-06-10 02:48:02 +0200

    UI updates, refactor and fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import Main from "./components/main/main";
 import Rules from "./components/DevIsleRules/DevIsleRules";
 import Schedule from "./components/Schedule/Schedule";
@@ -11,40 +12,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navigation">
-          <img
-            src="https://avatars3.githubusercontent.com/u/48589214?s=200&v=4"
-            width="75"
-            height="75"
-            alt="Dev Isle"
-          />
-          <ul>
-            <li>
-              <NavLink to="/" exact activeStyle={{ color: "#65e0ff" }}>
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/rules" activeStyle={{ color: "#65e0ff" }}>
-                Rules
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/schedule" activeStyle={{ color: "#65e0ff" }}>
-                Schedule
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects" activeStyle={{ color: "#65e0ff" }}>
-                Projects
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Route path="/" exact component={Main} />
-        <Route path="/rules" component={Rules} />
-        <Route path="/schedule" component={Schedule} />
-        <Route path="/projects" component={Projects} />
+        <Navbar />
+        <Route exact path="/" component={Main} />
+        <Route exact path="/rules" component={Rules} />
+        <Route exact path="/schedule" component={Schedule} />
+        <Route exact path="/projects" component={Projects} />
       </div>
     </Router>
   );
