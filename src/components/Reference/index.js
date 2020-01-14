f51aef490f79783c4e756eb304a93b312927675d
jonathan <jonathanprada@protonmail.ch>
2020-01-14 20:34:26 +0100

    added demo examples in reference-js documentation page

--- src/components/Reference/index.js
+++ src/components/Reference/index.js
@@ -72,32 +72,43 @@ const Reference = () => {
         <pre>
           <code>
             {`
-  {
-    "references": [
+{
+  "total": 3,
+  "references": [
       {
-        "type": "ARTICLE",
-        "key": "smit54",
-        "AUTHOR": "J. G. Smith and H. K. Weston",
-        "TITLE": "Nothing Particular in this Years History",
-        "YEAR": "1954",
-        "JOURNAL": "J. Geophys. Res.",
-        "VOLUME": "2",
-        "PAGES": "14-15"
+          "type": "article",
+          "key": "smit54",
+          "title": "Nothing Particular in this Years History",
+          "author": "J. G. Smith and H. K. Weston",
+          "year": "1954",
+          "journal": "J. Geophys. Res.",
+          "volume": "2",
+          "pages": "14-15"
       },
       {
-        "type": "BOOK",
-        "key": "colu92",
-        "AUTHOR": "Christopher Columbus",
-        "TITLE": "How I Discovered America",
-        "YEAR": "1492",
-        "PUBLISHER": "Hispanic Press",
-        "ADDRESS": "Barcelona"
+          "type": "book",
+          "key": "colu92",
+          "title": "How I Discovered America",
+          "author": "Christopher Columbus",
+          "year": "1492",
+          "publisher": "Hispanic Press",
+          "address": "Barcelona"
       }
-    ]
-  }  
+  ]
+} 
             `}
           </code>
         </pre>
+        <br />
+        <h5>Demo</h5>
+        <iframe
+          src="https://codesandbox.io/embed/confident-http-zdbhf?fontsize=14&hidenavigation=1&theme=dark"
+          styles="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+          title="confident-http-zdbhf"
+          allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
+          sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+        ></iframe>
+        <br />
         <br />
         <h4>JSON to Bibtex</h4>
         <span>
@@ -142,6 +153,15 @@ const Reference = () => {
             <br />
           </code>
         </pre>
+        <br />
+        <h5>Demo</h5>
+        <iframe
+          src="https://codesandbox.io/embed/adoring-yalow-2r1dw?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
+          styles="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+          title="JSON To Bibtex"
+          allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
+          sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+        ></iframe>
       </div>
     </div>
   );
